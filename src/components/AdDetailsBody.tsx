7128b118e7095c481cc67863276eae4feb60632c
George W F Santos <gweverton21@hotmail.com>
2023-03-17 21:27:42 -0300

    clean some unused imports

--- src/components/AdDetailsBody.tsx
+++ src/components/AdDetailsBody.tsx
@@ -14,15 +14,10 @@ import {
 import { Feather } from "@expo/vector-icons";
 
 import { AdDescription } from "./AdDescription";
-import { Header } from "./Header";
 
-import adImage from "../assets/bike.png";
-import { ArrowLeft } from "phosphor-react-native";
 import { AdImageSlider } from "./AdImageSlider/AdImageSlider";
 
 export function AdDetailsBody() {
-  const { colors } = useTheme();
-
   return (
     <>
       <AdImageSlider />
