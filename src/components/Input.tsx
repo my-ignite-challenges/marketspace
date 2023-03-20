e848c1c286e20304ec137716bc5031d5a837be87
George W F Santos <gweverton21@hotmail.com>
2023-03-20 12:40:15 -0300

    refactor Input for form validation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,40 @@
-import { IInputProps, Input as NativeBaseInput } from "native-base";
+import {
+  FormControl,
+  IInputProps,
+  Input as NativeBaseInput,
+} from "native-base";
 
-type Props = IInputProps & {};
+type Props = IInputProps & {
+  errorMessage?: string | null;
+};
 
-export function Input({ ...props }: Props) {
+export function Input({ errorMessage = null, isInvalid, ...props }: Props) {
+  const hasErrors = !!errorMessage || isInvalid;
+
   return (
-    <NativeBaseInput
-      bgColor="gray.100"
-      px={4}
-      fontSize="md"
-      w="full"
-      h="45px"
-      placeholderTextColor="gray.400"
-      borderColor="transparent"
-      borderRadius="6px"
-      _focus={{
-        borderWidth: 1,
-        borderColor: "blue.500",
-      }}
-      {...props}
-    />
+    <FormControl isInvalid={hasErrors} mb={4}>
+      <NativeBaseInput
+        bgColor="gray.100"
+        px={4}
+        fontSize="md"
+        w="full"
+        h="45px"
+        placeholderTextColor="gray.400"
+        isInvalid={hasErrors}
+        borderColor="transparent"
+        borderRadius="6px"
+        _invalid={{
+          borderWidth: 1,
+          borderColor: "red.500",
+        }}
+        _focus={{
+          borderWidth: 1,
+          borderColor: "blue.500",
+        }}
+        {...props}
+      />
+
+      <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
   );
 }
