32ee80f7bd5f7906e531ad81ef1b67bc07db4bbf
George W F Santos <gweverton21@hotmail.com>
2023-03-24 22:10:24 -0300

    create ProductImagePicker component and its functionality

--- src/components/AdForm.tsx
+++ src/components/AdForm.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
 import {
   useTheme,
   Text,
   VStack,
   Pressable,
   HStack,
+  Image,
   TextArea,
   Radio,
   Switch,
   Checkbox,
   ScrollView,
+  useToast,
 } from "native-base";
 import { ArrowLeft, Plus, X } from "phosphor-react-native";
 
@@ -20,17 +24,137 @@ import { Input } from "../components/Input";
 
 import { paymentMethods } from "../utils";
 
-import adImage from "../assets/bike.png";
+type ProductImage = {
+  name: string;
+  uri: string;
+  type: string;
+};
+
+type ProductImagePickerProps = {
+  uri?: string;
+  imageIndex?: number;
+};
 
 export function AdForm() {
   const [productCondition, setProductCondition] = useState("");
   const [selectedPaymentMethods, setSelectedPaymentMethods] = useState([]);
+  const [selectedImages, setSelectedImages] = useState<ProductImage[]>([]);
 
   const data = {} as any;
 
   const { colors } = useTheme();
+  const toast = useToast();
 
-  const isImageSelected = false; // => temporary
+  async function handleProductImagesSelection() {
+    try {
+      console.log("clicked");
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      console.log(selectedPhoto);
+
+      if (selectedPhoto.canceled) {
+        return;
+      }
+
+      if (selectedPhoto.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          selectedPhoto.assets[0].uri
+        );
+
+        if (
+          photoInfo.exists &&
+          photoInfo.size &&
+          photoInfo.size / 1024 / 1024 > 5
+        ) {
+          return toast.show({
+            title:
+              "O tamanho da imagem selecionada excede o limite de 5MB. Selecione uma imagem menor.",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        const fileExtension = selectedPhoto.assets[0].uri.split(".").pop();
+
+        const selectedPhotoFile = {
+          name: `mypicture.${fileExtension}`.toLowerCase(),
+          uri: selectedPhoto.assets[0].uri,
+          type: `${selectedPhoto.assets[0].type}/${fileExtension}`,
+        } as any;
+
+        if (selectedImages.length <= 2) {
+          setSelectedImages([...selectedImages, selectedPhotoFile]);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleProductImageRemoval(imageIndex: number | undefined) {
+    const filteredSelectedImages = selectedImages.filter(
+      (_, index) => imageIndex !== index
+    );
+
+    setSelectedImages(filteredSelectedImages);
+  }
+
+  function ProductImagePicker({ uri, imageIndex }: ProductImagePickerProps) {
+    return (
+      <Pressable
+        bgColor="gray.300"
+        w="100px"
+        h="100px"
+        alignItems="center"
+        justifyContent="center"
+        _pressed={{ opacity: 0.8 }}
+        borderRadius="6px"
+        position="relative"
+        onPress={handleProductImagesSelection}
+        disabled={!!uri}
+        zIndex={1}
+      >
+        <Plus color={colors.gray[400]} size={24} />
+
+        {uri && (
+          <Image
+            source={{ uri }}
+            alt="Imagem do Produto"
+            w="100px"
+            h="100px"
+            position="absolute"
+            top={0}
+            left={0}
+            resizeMode="contain"
+            borderRadius="6px"
+          />
+        )}
+
+        {uri && (
+          <Pressable
+            w={4}
+            h={4}
+            rounded="full"
+            bgColor="gray.600"
+            alignItems="center"
+            justifyContent="center"
+            position="absolute"
+            top={1}
+            right={1}
+            onPress={() => handleProductImageRemoval(imageIndex)}
+            zIndex={2}
+          >
+            <X size={12} color={colors.gray[100]} />
+          </Pressable>
+        )}
+      </Pressable>
+    );
+  }
 
   return (
     <VStack bgColor="gray.200" flex={1}>
@@ -46,38 +170,20 @@ export function AdForm() {
             Imagens
           </Text>
           <Text color="gray.500">
-            Escolha até 3 imagens para mostrar o quando o seu produto é
+            Escolha até 3 imagens para mostrar o quanto o seu produto é
             incrível!
           </Text>
 
-          <HStack mt={4} w="full">
-            <Pressable
-              bgColor="gray.300"
-              w="100px"
-              h="100px"
-              alignItems="center"
-              justifyContent="center"
-              _pressed={{ opacity: 0.8 }}
-              borderRadius="6px"
-              position="relative"
-            >
-              <Plus color={colors.gray[400]} size={24} />
-              {isImageSelected && (
-                <Pressable
-                  w={4}
-                  h={4}
-                  rounded="full"
-                  bgColor="gray.600"
-                  alignItems="center"
-                  justifyContent="center"
-                  position="absolute"
-                  top={1}
-                  right={1}
-                >
-                  <X size={12} color={colors.gray[100]} />
-                </Pressable>
-              )}
-            </Pressable>
+          <HStack mt={4} w="full" space={2}>
+            {selectedImages.map((image, index) => (
+              <ProductImagePicker
+                uri={selectedImages[index]?.uri}
+                key={image.uri}
+                imageIndex={index}
+              />
+            ))}
+
+            {selectedImages.length < 3 && <ProductImagePicker />}
           </HStack>
 
           <VStack mt={8}>
